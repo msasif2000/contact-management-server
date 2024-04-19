ea3372d226e772b2903d7613b097f48f385ea9c1
Asif <msasifiiuc@gmail.com>
2024-04-19 23:18:40 +0600

    link set up done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://cellio-contact.web.app'],
 }));
 app.use(express.json());
 
